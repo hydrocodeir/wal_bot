e7488b0af40a51ffcd7f645098d6a96bd58fa4be
primeZdev <the.primez.dev@gmail.com>
2025-02-09 12:34:04 +0330

    env path

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from telebot import TeleBot
 import os
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__),'db', '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__),'..', '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 
